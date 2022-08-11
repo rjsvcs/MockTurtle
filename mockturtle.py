cca54be0e0ebb8b625d72e2b355e8215bb729909
Bobby St. Jacques <rjs@se.rit.edu>
2022-08-11 17:20:21 -0400

    implementing initial version of mockturtle

---
+++ mockturtle.py
@@ -0,0 +1,49 @@
+import turtle
+
+"""
+Turtle functions.
+"""
+def forward(distance):
+    turtle.forward(distance)
+
+def fd(distance):
+    turtle.fd(distance)
+
+def back(distance):
+    turtle.back(distance)
+
+def bk(distance):
+    turtle.bk(distance)
+
+def backward(distance):
+    turtle.backward(distance)
+
+def right(angle):
+    turtle.right(angle)
+
+def rt(angle):
+    turtle.rt(angle)
+
+def left(angle):
+    turtle.left(angle)
+
+def lt(angle):
+    turtle.lt(angle)
+
+def goto(x, y=None):
+    turtle.goto(x, y)
+
+def setpos(x, y=None):
+    turtle.setpos(x, y)
+
+def setposition(x, y=None):
+    turtle.setposition(x, y)
+
+def setx(x):
+    turtle.setx(x)
+
+def sety(y):
+    turtle.sety(y)
+
+
+
