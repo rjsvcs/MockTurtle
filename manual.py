bc5442b45d0994fea6609164eb96b4fd77d921fb
Bobby St. Jacques <rjs@se.rit.edu>
2022-08-11 17:39:50 -0400

    adding more turtle functions

---
+++ manual.py
@@ -0,0 +1,9 @@
+import mockturtle as t
+#import turtle as t
+
+def main():
+    t.dot(5, "blue")
+    input("Press enter to continue...")
+
+if __name__ == "__main__":
+    main()
--- mockturtle.py
+++ mockturtle.py
@@ -1,6 +1,13 @@
 import turtle
 
 """
+Mock functions
+"""
+
+def expect(name, *args):
+    pass
+
+"""
 Turtle functions.
 """
 def forward(distance):
@@ -44,6 +51,48 @@ def setx(x):
 
 def sety(y):
     turtle.sety(y)
+
+def setheading(to_angle):
+    turtle.setheading(to_angle)
+
+def seth(to_angle):
+    turtle.seth(to_angle)
+
+def home():
+    turtle.home()
+
+def circle(radius, extent=None, steps=None):
+    turtle.circle(radius, extent, steps)
+
+def dot(size=None, *color):
+    turtle.dot(size, *color)
+
+def stamp():
+    return turtle.stamp()
+
+def clearstamp(stampid):
+    turtle.clearstamp(stampid)
+
+def clearstamps(n=None):
+    turtle.clearstamps(n)
+
+def undo():
+    turtle.undo()
+
+def speed(speed=None):
+    return turtle.speed(speed)
+
+def position():
+    return turtle.position()
+
+def pos():
+    return turtle.pos()
+
+def towards(x, y=None):
+    turtle.towards(x, y)
+
+
+
 
 
 
